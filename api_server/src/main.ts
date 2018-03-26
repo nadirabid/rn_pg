22a7a5de518289c21531a065b7d1b52d54e8901b
Nadir Muzaffar <nadirabid@gmail.com>
2018-03-25 22:44:49 -0700

    setup config based management - yay 12 factor

--- api_server/src/main.ts
+++ api_server/src/main.ts
@@ -1,3 +1,31 @@
-import express = require('express');
+import * as express from 'express';
+import * as http from 'http';
+import * as convict from 'convict';
+import * as path from 'path';
 
-console.log('tryping this out');
+// setup config
+const config: convict.Config = convict.default({
+    port: {
+        'arg': 'port',
+        'doc': 'The port the server should listen at.',
+        'default': null,
+        'env': 'PORT',
+        'format': 'port',
+    }
+});
+
+config.loadFile(path.join(__dirname, '../conf/development.json'));
+config.validate();
+
+// create app
+const app: express.Express = express.default();
+
+// start server
+const server: http.Server = http.createServer(app);
+server.listen(3000);
+server.on('listening', () => {
+    console.log('Server listening...');
+});
+server.on('error', (err: Error) => {
+    console.error('Server crapped out: ', err);
+});
