9943f28b5e7ca1749256fdd801e2b7c73635c58a
Nadir Muzaffar <nadirabid@gmail.com>
2018-03-28 00:16:40 -0700

    stepping it up

--- api_server/app/graphql/schema.ts
+++ api_server/app/graphql/schema.ts
@@ -1,15 +1,30 @@
 import { buildSchema} from 'graphql';
 
-// Construct a schema, using GraphQL schema language
-export const schema = buildSchema(`
+export var schema = buildSchema(`
+  type User {
+     id: Int!
+     firstName: String
+     lastName: String
+  }
+  
   type Query {
-    hello: String
+    
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
-export const root = {
-    hello: () => {
-        return 'Hello world!';
+export var root = {
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
     },
 };
+
