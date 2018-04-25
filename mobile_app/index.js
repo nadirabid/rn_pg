df17002ff1673d6cba27f377bae409729eed6460
Nadir Muzaffar <nadirabid@gmail.com>
2018-04-25 01:04:37 -0700

    make things work. again?

--- mobile_app/index.js
+++ mobile_app/index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
+import { AppRegistry } from 'react-native'
+import App from './src/App'
 
-AppRegistry.registerComponent('mobile_app', () => App);
+AppRegistry.registerComponent('mobile_app', () => App)
