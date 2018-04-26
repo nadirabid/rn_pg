81dfdc533fb8ebd71b52c92481ba06cfa86e39b3
Nadir Muzaffar <nadirabid@gmail.com>
2018-04-25 22:22:46 -0700

    Got react-native-navigation workinggggg

--- mobile_app/index.js
+++ mobile_app/index.js
@@ -3,7 +3,7 @@ import { AppRegistry } from 'react-native'
 import App, { Test } from './src/App'
 
 // AppRegistry.registerComponent('mobile_app', () => App)
-Navigation.registerComponent('app.main', () => Test)
+Navigation.registerComponent('app.main', () => App)
 
 Navigation.startSingleScreenApp({
     screen: {
